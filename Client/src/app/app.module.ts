25acd8d9d046837013cf0f085a9e00cd894ca714
Dopeysmith <danny@dannysmith.com>
2017-11-12 19:24:40 -0500

    Added BreakRoomComponent to the AppModule

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { BreakRoomComponent } from './components/break-room/break-room.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BreakRoomComponent
   ],
   imports: [
     BrowserModule
